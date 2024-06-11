b600dbc2285d2057404c89805d47db6146ae2f9c
amir-38 <a_saruxanov_38@mail.ru>
2024-06-11 15:42:18 +0400

    add responsive scss

--- kiddy/src/components/reusibleComponents/button/Button.jsx
+++ kiddy/src/components/reusibleComponents/button/Button.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ color, padding, children, width }) => {
+const Button = ({
+  color = { backgroundColor: "#007bff", textColor: "#fff" },
+  padding = "10px 20px",
+  width = "150px",
+  children,
+}) => {
   const buttonStyle = {
     padding: padding,
     borderRadius: "18px 0px 18px 0px",
@@ -25,15 +30,6 @@ Button.propTypes = {
   width: PropTypes.string,
   padding: PropTypes.string,
   children: PropTypes.node.isRequired,
-};
-
-Button.defaultProps = {
-  color: {
-    backgroundColor: "#007bff",
-    textColor: "#fff",
-  },
-  padding: "10px 20px",
-  width: "150px",
 };
 
 export default Button;
